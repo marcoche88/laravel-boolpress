9b50cde3527a8fa4d9e453253c29c58a6caf0b1a
marcoche88 <marco.che@hotmail.it>
2021-10-21 16:12:16 +0200

    added relations Post and Category model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'content', 'image'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
