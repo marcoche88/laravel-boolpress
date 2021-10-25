6bf39ec37957bbbc7394d4b56be7e84738c9ea89
marcoche88 <marco.che@hotmail.it>
2021-10-25 17:21:09 +0200

    added tags in post show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,12 @@
         <p>{{ $post->content }}</p>
         <address>{{ $post->created_at }}</address>
         <p>Categoria: @if($post->category) {{ $post->category->name }} @else - @endif</p>
+        <p>Tags:
+            @forelse ($post->tags as $tag)
+            #{{ $tag->name }} 
+        @empty
+            -
+        @endforelse</p>
         <div class="d-flex justify-content-end">
             <a href="{{ route('admin.posts.index') }}" class="btn btn-primary">Indietro</a>
             <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-secondary mx-2">Modifica</a>
